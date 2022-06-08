86edee6c281b2f1f10350f2b2b0d8919c9c994ad
aryaman31 <aryamanarora31@gmail.com>
2022-06-08 15:30:15 +0100

    initial commit

---
+++ src/test/java/com/example/gp_scheduling/GpSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gp_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
