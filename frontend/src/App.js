c2bb4bf50d9c6b24d7d588125441179fcf191918
aryaman31 <aryamanarora31@gmail.com>
2022-06-09 22:29:54 +0100

    Testing actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Also ur mom.
+          Edit <code>src/App.js</code> and save to reload. Also ur mom. is kinda hawt. 
         </p>
         <a
           className="App-link"
