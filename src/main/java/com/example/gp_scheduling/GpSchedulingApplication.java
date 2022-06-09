d08cdfd9822db86fb3b26e4927ce083605038d88
aryaman31 <aryamanarora31@gmail.com>
2022-06-09 21:38:04 +0100

    Added support for react apps to displayed statically at root

--- src/main/java/com/example/gp_scheduling/GpSchedulingApplication.java
+++ src/main/java/com/example/gp_scheduling/GpSchedulingApplication.java
@@ -2,20 +2,19 @@ package com.example.gp_scheduling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class GpSchedulingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GpSchedulingApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s !", name);
-	}
+	// @RequestMapping(value="/")
+	// public String index() {
+	// 	return "index";
+	// }
 }
